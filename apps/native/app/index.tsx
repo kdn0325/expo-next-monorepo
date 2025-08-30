67beee266e23fcadcd424631cf8da349b22b6f35
kdn0325 <dn10003@gmail.com>
2025-08-31 01:23:15 +0900

    feat: refactor Index component to use Tailwind CSS classes for layout

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -3,13 +3,7 @@ import { View } from "ui/view";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
